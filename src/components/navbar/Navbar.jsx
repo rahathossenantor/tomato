fc9cd8df8fc1e95c715dc5f915aa34824d9cc76c
Md Rahat Hossen Antor <md.rahathossenantor@gmail.com>
2025-07-18 20:47:53 +0600

    Initial project setup and navbar srtucture are completed

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import { assets } from "../../assets/assets";
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <img src={assets.logo} alt="Logo" className="logo" />
+            <ul className="navbar-menu">
+                <li>Home</li>
+                <li>Menu</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+
+            <div className="navbar-right">
+                <img src={assets.search_icon} alt="" />
+                <div className="navbar-search-icon">
+                    <img src={assets.basket_icon} alt="" />
+                    <div className="dot"></div>
+                </div>
+                <button>Sign In</button>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
