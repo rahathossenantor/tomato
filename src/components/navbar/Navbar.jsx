5af40d3e980fe4987d96cf6904c1e35f123a8a6a
Md Rahat Hossen Antor <md.rahathossenantor@gmail.com>
2025-07-25 20:04:43 +0600

    Navbar completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,23 @@
+import { useState } from "react";
 import { assets } from "../../assets/assets";
 import "./Navbar.css";
 
 const Navbar = () => {
+    const [menu, setMenu] = useState("Home");
+
     return (
         <div className="navbar">
             <img src={assets.logo} alt="Logo" className="logo" />
-            <ul className="navbar-menu">
-                <li>Home</li>
-                <li>Menu</li>
-                <li>Services</li>
-                <li>Contact</li>
+            <ul className="navbar_menu">
+                <li className={menu === "Home" ? "active" : ""} onClick={() => setMenu("Home")}>Home</li>
+                <li className={menu === "Menu" ? "active" : ""} onClick={() => setMenu("Menu")}>Menu</li>
+                <li className={menu === "Services" ? "active" : ""} onClick={() => setMenu("Services")}>Services</li>
+                <li className={menu === "Contact" ? "active" : ""} onClick={() => setMenu("Contact")}>Contact</li>
             </ul>
 
-            <div className="navbar-right">
+            <div className="navbar_right">
                 <img src={assets.search_icon} alt="" />
-                <div className="navbar-search-icon">
+                <div className="navbar_search_icon">
                     <img src={assets.basket_icon} alt="" />
                     <div className="dot"></div>
                 </div>
